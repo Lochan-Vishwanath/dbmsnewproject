f8bd6bebc7496e4f168f621aafc7fbd69e7be0c2
Sanjay Bhakta <sanjaybhakta01@gmail.com>
2018-10-29 23:57:50 +0530

    add page
    created a new add class with updation of new movies into  the database

--- Java programs/Movie Database/src/Login.java
+++ Java programs/Movie Database/src/Login.java
@@ -13,7 +13,12 @@ import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
+import javax.swing.UIManager;
 
 public class Login extends JFrame 
 {
@@ -47,45 +52,63 @@ public class Login extends JFrame
 	 */
 	public Login() 
 	{
+		
+		
+		//Database Connection//
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/movie","root","");  
+			System.out.println("Connection established");
+			//here movie is database name, root is username and password  //
+			
+			//Statement Execution//
+			Statement stmt = con.createStatement( ); 
+			ResultSet rs=((java.sql.Statement) stmt).executeQuery("select Movie_title from movies where Movie_language='Hindi'");  
+			while(rs.next())    
+			System.out.println(rs.getString(1)); //+""+rs.getString(2)+"  "+rs.getString(3)//
+			
+			con.close();  
+			}catch(Exception e){ System.out.println(e);}
+		
+		
+		
+		
+			  
 		setBackground(Color.GREEN);
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Sanjay Bhakta\\Desktop\\movie\\objects-17-512.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 977, 591);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 99, 71));
+		contentPane.setBackground(new Color(0, 250, 154));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		
 		//Title Movie Database//
 		JLabel lblMovieDatabase = new JLabel("MOVIE DATABASE");
-		lblMovieDatabase.setFont(new Font("Sitka Text", Font.PLAIN, 28));
+		lblMovieDatabase.setFont(new Font("Segoe UI Semilight", Font.BOLD, 28));
 		lblMovieDatabase.setForeground(new Color(65, 105, 225));
-		lblMovieDatabase.setBounds(238, 13, 293, 61);
+		lblMovieDatabase.setBounds(179, 13, 293, 61);
 		contentPane.add(lblMovieDatabase);
 		
 		//Label Search//
 		JLabel lblSearch = new JLabel("Search:");
 		lblSearch.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblSearch.setBounds(37, 93, 86, 36);
+		lblSearch.setBounds(23, 94, 86, 36);
 		contentPane.add(lblSearch);
 		
 		textField = new JTextField();
-		textField.setBounds(151, 94, 419, 36);
+		textField.setBounds(121, 97, 419, 36);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		//Search button//
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.setIcon(new ImageIcon("C:\\Users\\Sanjay Bhakta\\Desktop\\movie\\sear.png"));
-		btnNewButton.setBounds(591, 90, 52, 48);
+		btnNewButton.setBounds(571, 97, 39, 36);
 		contentPane.add(btnNewButton);
-		
-		//Panel//
-		JPanel panel = new JPanel();
-		panel.setBounds(12, 13, 683, 516);
-		contentPane.add(panel);
-		panel.setVisible(false);
 		
 		//Venom//
 		JButton btnNewButton_1 = new JButton("New button");
@@ -119,6 +142,22 @@ public class Login extends JFrame
 		btnNewButton_3.setIcon(new ImageIcon("C:\\Users\\Sanjay Bhakta\\Desktop\\movie\\first.png"));
 		btnNewButton_3.setBounds(499, 226, 176, 270);
 		contentPane.add(btnNewButton_3);
+		
+		
+		
+		//Add a movie Button//
+		JButton btnAddAMovie = new JButton("Add a Movie");
+		btnAddAMovie.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				Add a=new Add();
+				a.setVisible(true);
+				
+			}
+		});
+		btnAddAMovie.setBounds(654, 103, 122, 24);
+		contentPane.add(btnAddAMovie);
 		
 		
 	}
